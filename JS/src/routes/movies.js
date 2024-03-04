5f254d9d6b427dc08ec3e28639654662d06d3abb
AlanCano123 <alancano123@example.com>
2024-03-04 10:10:41 -0300

    ..

--- JS/src/routes/movies.js
+++ JS/src/routes/movies.js
@@ -6,59 +6,171 @@ const paymentAccounts = [
         "deudas": [
             {
                 "NumeroLegal": "A-123-12345678",
-                "FechaEmision": "2014-01-01"
+                "FechaEmision": "2014-01-01",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "A-321-12545678",
-                "FechaEmision": "2013-01-01"
+                "FechaEmision": "2013-01-01",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "A-213-12543678",
-                "FechaEmision": "2020-01-01"
+                "FechaEmision": "2020-01-01",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "A-432-21345678",
-                "FechaEmision": "2014-01-01"
+                "FechaEmision": "2014-01-01",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-456-78901234",
-                "FechaEmision": "2015-02-02"
+                "FechaEmision": "2015-02-02",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-789-12345678",
-                "FechaEmision": "2016-03-03"
+                "FechaEmision": "2016-03-03",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-123-45678901",
-                "FechaEmision": "2017-04-04"
+                "FechaEmision": "2017-04-04",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-234-56789012",
-                "FechaEmision": "2018-05-05"
+                "FechaEmision": "2018-05-05",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-345-67890123",
-                "FechaEmision": "2019-06-06"
+                "FechaEmision": "2019-06-06",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-456-78901234",
-                "FechaEmision": "2020-07-07"
+                "FechaEmision": "2020-07-07",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-567-89012345",
-                "FechaEmision": "2021-08-08"
+                "FechaEmision": "2021-08-08",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-678-90123456",
-                "FechaEmision": "2022-09-09"
+                "FechaEmision": "2022-09-09",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-789-01234567",
-                "FechaEmision": "2023-10-10"
+                "FechaEmision": "2023-10-10",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-890-12345678",
-                "FechaEmision": "2024-11-11"
+                "FechaEmision": "2024-11-11",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             }
         ]
     },
@@ -67,52 +179,148 @@ const paymentAccounts = [
         "detalles": [
             {
                 "NumeroLegal": "B-456-78901234",
-                "FechaEmision": "2015-02-02"
+                "FechaEmision": "2015-02-02",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-789-12345678",
-                "FechaEmision": "2016-03-03"
+                "FechaEmision": "2016-03-03",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-456-78901234",
-                "FechaEmision": "2015-02-02"
+                "FechaEmision": "2015-02-02",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-789-12345678",
-                "FechaEmision": "2016-03-03"
+                "FechaEmision": "2016-03-03",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-123-45678901",
-                "FechaEmision": "2017-04-04"
+                "FechaEmision": "2017-04-04",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-234-56789012",
-                "FechaEmision": "2018-05-05"
+                "FechaEmision": "2018-05-05",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-345-67890123",
-                "FechaEmision": "2019-06-06"
+                "FechaEmision": "2019-06-06",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             
             {
                 "NumeroLegal": "B-456-78901234",
-                "FechaEmision": "2020-07-07"
+                "FechaEmision": "2020-07-07",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-567-89012345",
-                "FechaEmision": "2021-08-08"
+                "FechaEmision": "2021-08-08",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-678-90123456",
-                "FechaEmision": "2022-09-09"
+                "FechaEmision": "2022-09-09",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-789-01234567",
-                "FechaEmision": "2023-10-10"
+                "FechaEmision": "2023-10-10",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             },
             {
                 "NumeroLegal": "B-890-12345678",
-                "FechaEmision": "2024-11-11"
+                "FechaEmision": "2024-11-11",
+                "FechaVencimiento": "2023-01-02",
+                "Moneda": "pesos",
+                "ImporteEmision": 100,
+                "SaldoPendiente": 10,
+                "DiasDeGracia": 3,
+                "MarcaReclamo": "marcaGenerica",
+                "MotivoReclamo": "motivoDel Reclamo",
+                "IdComprobante": 1
             }
         ]
     }
